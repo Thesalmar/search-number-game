6e3d92a33fc115c912460c6b896529f017235c0a
Thesalmar <harry0995@gmail.com>
2018-02-10 15:27:16 +0100

    Not more NULL conditions and adding Readme

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿## Search Number Game
+
+Usage : ./program
+
+Example :
+
+    (randomly chose 84)
+    
+    Try a number: 44
+    44
+    Program : +
+    Try a number: 63
+    63
+    Program : +
+    Try a number: 77
+    77
+    Program : +
+    Try a number: 98
+    98
+    Program : -
+    Try a number: 84
+    84
+    Program: You win in 5 step(s) 
+
--- src/search-number-game.c
+++ src/search-number-game.c
@@ -12,23 +12,19 @@
 
 void error_check(int number)
 {
-	if (number == NULL)
-		write(2, "Error : Number is NULL\n", 23);
-	else if (number < 1 || number > 100)
+	if (number < 1 || number > 100)
 		write(2, "Error : Please input a number between 1 and 100\n", 48);
 }
 
 int check_answer(int rand_number, int answer, int step)
 {
-	if (answer == 0 || answer == NULL)
-		return (0);
-	else if (answer > rand_number)
+	if (answer > rand_number)
 		write(1, "Program : -\n", 12);
 	else if (answer < rand_number)
 		write(1, "Program : +\n", 12);
 	else
 	{
-		printf("Program: You win in %d steps\n", step);
+		printf("Program: You win in %d step(s)\n", step);
 		return (1);
 	}
 	return (0);
@@ -39,7 +35,7 @@ int game()
 	int rand_number = (rand() % (MAX - MIN + 1)) + MIN;
 	int answer = 0;
 	int step = 0;
-	int check_win = check_answer(rand_number, answer, step);
+	int check_win = 0;
 
 	while (check_win != 1)
 	{
